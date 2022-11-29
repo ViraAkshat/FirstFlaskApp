3c75c89f84bfed1d5c2f21c28a0aa042e06c9042
Akshat Vira <akshatvira812@gmail.com>
2022-11-29 22:45:59 +0530

    Add delete and update functionality
    Added update.html which acts as update page for the task
    Added snippets for handling POST, handles update and delete functions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FirstFlaskApp
 This is my first attempt at creating a Flask app.
+
+# Things learnt:
+1) Creating and starting a virtual environment
+Creation:
+    python -m .venv
+Initializing:
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+    .\.venv\Scripts\activate
+
+2) Jinja2 
+Jinja2 is used for template inheritance
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,9 +17,46 @@ class Todo(db.Model):
         ### returns the task's id
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])   # Route decorator
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']  # input tag with name=content
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()   # looks at all created tasks, sorts by date and returns the list
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem in updating the task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
